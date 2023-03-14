cc2e47eef4994af928dc9f73698162fff7e61508
Gabriel Aguiar Dantas <gabrielaguiardantas@yahoo.com.br>
2023-03-14 10:59:34 -0300

    aula 4.3 concluída

---
+++ Trybe-exercícios/Back-end/seção-04-introdução-ao-desenvolvimento-web-com-nodejs/dia-03-nodejs-testes-de-integração/cacau-trybe/src/cacauTrybe.js
@@ -0,0 +1,78 @@
+// src/cacauTrybe.js
+
+const fs = require('fs').promises;
+const { join } = require('path');
+const path = '/files/cacauTrybeFile.json';
+
+
+const readCacauTrybeFile = async () => {
+
+  try {
+    const contentFile = await fs.readFile(join(__dirname, path), 'utf-8');
+    return JSON.parse(contentFile);
+  } catch (error) {
+    return null;
+  }
+};
+
+const getAllChocolates = async () => {
+  const cacauTrybe = await readCacauTrybeFile();
+  return cacauTrybe.chocolates;
+};
+
+const getChocolateById = async (id) => {
+    const cacauTrybe = await readCacauTrybeFile();
+    return cacauTrybe.chocolates
+      .find((chocolate) => chocolate.id === id);
+  };
+  
+  const getChocolatesByBrand = async (brandId) => {
+    const cacauTrybe = await readCacauTrybeFile();
+    return cacauTrybe.chocolates
+      .filter((chocolate) => chocolate.brandId === brandId);
+  };
+
+  // exercício 4
+  const getChocolatesByName = async (name) => {
+    const cacauTrybe = await readCacauTrybeFile();
+    return cacauTrybe.chocolates
+      .filter((chocolate) => chocolate.name.toLowerCase().includes(name.toLowerCase()));
+  };
+
+  // exercício 6
+  const writeCacauTrybeFile = async (content) => {
+    try {
+      const completePath = join(__dirname, path);
+      await fs.writeFile(completePath, JSON.stringify(content, null, 2));
+    } catch (e) {
+      console.error('Erro ao salvar o arquivo', e.message);
+      return null;
+    }
+  };
+
+  const updateChocolate = async (id, update) => {
+    const cacauTrybe = await readCacauTrybeFile();
+    const chocolateToUpdate = cacauTrybe.chocolates.find(
+      (chocolate) => chocolate.id === id,
+    );
+  
+    if (chocolateToUpdate) {
+      cacauTrybe.chocolates = cacauTrybe.chocolates.map((chocolate) => {
+          if (chocolate.id === id) return { ...chocolate, ...update };
+          return chocolate;
+        });
+    
+      await writeCacauTrybeFile(cacauTrybe);
+      return { ...chocolateToUpdate, ...update };
+    }
+  
+    return false;
+  };
+
+module.exports = {
+    getAllChocolates,
+    getChocolateById,
+    getChocolatesByBrand,
+    getChocolatesByName,
+    updateChocolate
+};
