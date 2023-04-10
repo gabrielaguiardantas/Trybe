6ea5f215a77c24ec17a67a4908b36a56d55a021d
Gabriel Aguiar Dantas <gabrielaguiardantas@yahoo.com.br>
2023-04-10 09:42:38 -0300

    aula 6.3 concluída

--- Trybe-exercícios/Back-end/seção-06-ORM-e-autenticação/dia-02-ORM-associations-1-1-e-1-n/exercise-app/src/services/employee.service.js
+++ Trybe-exercícios/Back-end/seção-06-ORM-e-autenticação/dia-02-ORM-associations-1-1-e-1-n/exercise-app/src/services/employee.service.js
@@ -1,4 +1,11 @@
 const { Address, Employee } = require('../models/');
+const Sequelize = require('sequelize');
+const config = require('../config/config');
+
+const env = process.env.NODE_ENV || 'development';
+
+// Ajustamos para usar a configuração correta para nosso ambiente
+const sequelize = new Sequelize(config[env]);
 
 const getAll = async () => {
   const users = await Employee.findAll({
@@ -15,4 +22,19 @@ const getById = async (id) => {
   return employee;
 };
 
-module.exports = { getAll, getById };
+const insert = async ({ firstName, lastName, age, city, street, number }) => {
+  const result = await sequelize.transaction(async (t) => {
+    const employee = await Employee.create({
+      firstName, lastName, age,
+    }, { transaction: t });
+
+    await Address.create({
+      city, street, number, employeeId: employee.id
+    }, { transaction: t });
+    return employee;
+  });
+
+  return result;
+};
+
+module.exports = { getAll, getById, insert };
