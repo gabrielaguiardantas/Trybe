d0fd68aaf396433b29393abb7ab21cd2db9f5353
gabrielaguiardantas <gabrielaguiardantas@gmail.com>
2023-08-14 15:34:46 -0300

    aula 4.1 terminada

---
+++ Trybe-exercícios/Python - Eletiva/seção-04-flask/dia-01-ambiente-e-primeira-api/projeto-flask/course_exercise.py
@@ -0,0 +1,26 @@
+# exercício 1
+
+"""Só setar a url para http://localhost:8000/jokes no TC
+e mudar o método para POST,
+passando no body um JSON com uma chave joke e o valor da piada."""
+
+# exercício 2
+
+""" 3 métodos get estão criados na app.py:
+    Getall: Devolve todas as piadas cadastradas ->
+    TC - URL: http://localhost:8000/jokes
+    Getrandom: Devolve uma piada aleatória ->
+    TC - URL: http://localhost:8000/jokes/random
+    Getid: Devolve uma piada específica pelo id ->
+    TC - URL: http://localhost:8000/jokes/<id>
+"""
+
+# exercício 3
+
+""" Passa no body um JSON com uma chave joke e o novo valor da piada. ->
+TC - URL: http://localhost:8000/jokes/<id> - Método: PUT"""
+
+# exercício 4
+
+""" Passa na url o id e troca pro método delete ->
+TC - URL: http://localhost:8000/jokes/<id> - Método: DELETE"""
